3313b08d448d46c060e67fa9781cdc1fdb1dd0b0
Beatriz Castro <luanabia400@hotmail.com>
2025-01-12 11:38:33 -0300

    feat : adicionado o arquivo com a massa de dados para os testes de movies

---
+++ tests/e2e/movies.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+const { faker} = require('@faker-js/faker');
+const {LandingPage} =require('../pages/landingPage')
+const {Toast} =require('../pages/components');
+const { request } = require('http');
+
+test('Deve cadastrar com sucesso um novo filme',async ({page})=>{
+
+})
