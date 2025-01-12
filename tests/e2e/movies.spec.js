fd9e9125374fff06b88202640ffb594a683981a9
Beatriz Castro <luanabia400@hotmail.com>
2025-01-12 17:08:56 -0300

    feat: adicionado teste de movies

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,9 +1,23 @@
 const { test, expect } = require('@playwright/test');
 const { faker} = require('@faker-js/faker');
-const {LandingPage} =require('../pages/landingPage')
-const {Toast} =require('../pages/components');
-const { request } = require('http');
+const {LoginPage} =require('../pages/loginPage')
+const {Toast} =require('../pages/components')
+const {MoviesPage} =require('../pages/MoviesPage')
 
+
+let loginPage
+let toast
+let moviesPage
+
+test.beforeEach(async({page})=>{
+loginPage = new LoginPage(page)
+toast = new Toast(page)
+moviesPage = new MoviesPage(page)
+})
 test('Deve cadastrar com sucesso um novo filme',async ({page})=>{
+    await loginPage.visit()
+    await loginPage.submit('admin@zombieplus.com','pwd123')
+    await moviesPage.isLoggerIn()
 
+    await moviesPage.create('Nome do filme', 'Teste', 'Empresa', '2023')
 })
